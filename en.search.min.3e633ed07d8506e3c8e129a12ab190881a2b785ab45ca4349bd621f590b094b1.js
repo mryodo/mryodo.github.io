b5ddcf65946d9c0b5a4eb94e742ec7582aed1850
Anton Savostianov <mryodo@MacBook-Pro-Anton.local>
2019-12-28 19:34:48 +0300

    Initial commit

---
+++ en.search.min.3e633ed07d8506e3c8e129a12ab190881a2b785ab45ca4349bd621f590b094b1.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.afa6ae64c6634abaf7b47c15e49da304015f6df6d2e23375cbcbc97fb8c68ca1.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
